4db46f67d9a75d2da927b607dffacb73845307ce
Zachary Rais-Norman <zraisnorman@gmail.com>
2020-12-11 22:52:52 -0500

    remove the scroller - it sucks

--- src/App.svelte
+++ src/App.svelte
@@ -26,31 +26,6 @@ const reduceMotion = window.matchMedia("(prefers-reduced-motion: reduce)").match
 		<h2>
 			I have a versatile toolbox collected from a decade of varied work and a love of side projects. I enjoy playing with the intersections of some of those tools to find delightful new ways to communicate.
 		</h2>
-		<!-- <h2>Here's a bunch of words I know things about:</h2> -->
-		<!-- create a scroll of skills etc. -->
-		<div class="scrollWrapper sc">
-			<div class="scrollPusher">
-				<span>creative direction</span>
-				<span>branding</span>
-				<span>ux & ui design</span>
-				<span>illustration</span>
-				<span>animation</span>
-				<span>html/css/js</span>
-				<span>react/next</span>
-				<span>gatsby</span>
-				<span>svelte</span>
-				<span>gsap</span>
-				<span>canvas</span>
-				<span>d3</span>
-				<span>video</span>
-				<span>prototyping</span>
-				<span>wireframes</span>
-				<span>copy writing</span>
-				<span>editorial design</span>
-				<span>typography</span>
-				<span>package design</span>
-			</div>
-		</div>
 	</section>
 	<ProjectSection />
 	<section id="contact">
@@ -95,21 +70,4 @@ const reduceMotion = window.matchMedia("(prefers-reduced-motion: reduce)").match
 		align-items: center;
 		justify-content: center;
 	}
-
-	.scrollContainer {
-		width: 100%;
-		overflow: hidden;
-	}
-
-	.scrollPusher {
-		white-space: nowrap;
-		animation: marquee-horizontal 40s linear infinite;
-	}
-
-	.scrollPusher > span {
-		padding: 1em;
-		font-size: 3rem;
-	}
-
-	
 </style>
