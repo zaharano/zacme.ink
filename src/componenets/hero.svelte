054ebf2d78a5e095faaa3aa7ddb042780544c272
Zach Rais-Norman <Zach@smol.home>
2020-12-14 13:44:37 -0500

    added accent lines around speech bubble

--- src/componenets/hero.svelte
+++ src/componenets/hero.svelte
@@ -2,8 +2,9 @@
   import { onMount } from 'svelte';
   import { gsap } from "gsap";
   import { TextPlugin } from "gsap/TextPlugin";
+  import { DrawSVGPlugin } from 'gsap/all';
 
-  gsap.registerPlugin(TextPlugin);
+  gsap.registerPlugin(TextPlugin, DrawSVGPlugin);
 
   //reset tl function
   const resetTL = function() {
@@ -17,7 +18,8 @@
 
   //speech bubble TL
   const speechTL = gsap.timeline({delay: 1});
-
+  const lineTL = gsap.timeline({repeat: -1});
+  const startTL = gsap.timeline({delay: 2, repeat: -1})
 
   const delay = .02,
         duration = .01,
@@ -27,6 +29,49 @@
 
   onMount(() => {
     // animate speech bubble
+    gsap.set('.speech-outer-1', {
+      drawSVG: '34% 34%',
+      autoAlpha: 0,
+    });
+    gsap.set('.speech-outer-2', {
+      drawSVG: '1% 1%',
+      autoAlpha: 0,
+    });
+
+    const ltime = 3;
+
+    lineTL
+    .to('.speech-outer-1', {
+      drawSVG: '54% 65%',
+      duration: ltime,
+      ease: 'expo.in',
+      autoAlpha: 1,
+    })
+    .to('.speech-outer-1', {
+      drawSVG: '74% 74%',
+      duration: ltime,
+      ease: "expo.out",
+      autoAlpha: 0,
+    }, ">")
+    .to('.speech-outer-2', {
+      drawSVG: '5% 16%',
+      duration: ltime,
+      ease: 'expo.in',
+      autoAlpha: 1,
+    }, ".5")
+    .to('.speech-outer-2', {
+      drawSVG: '21% 21%',
+      duration: ltime,
+      ease: "expo.out",
+      autoAlpha: 0,
+    }, ">")
+    .set('.speech-outer-1', {
+      drawSVG: '34% 34%',
+    })
+    .set('.speech-outer-2', {
+      drawSVG: '1% 1%',
+    });
+
     speechTL.from('.speech', {
       opacity: 0,
       duration: .2,
@@ -66,12 +111,13 @@
       transformOrigin: "50%, 50%"
     }, "-=2.2");
 
+
     // track nudge count by dimension
     const nudge = function(dim) {
       nudges[dim]++;
     }
     // word 'designer' nudges up and left, guides show up, color picker appear, color changed, yoyo
-    const desSel = ' .designer';
+    const desSel = '.designer';
 
     desTL.set('.guide-y-ends', {
       autoAlpha: 0
@@ -244,41 +290,48 @@
 </script>
 
 <section>
-  <div class="speech">
-    <svg 
-      class="bubble" 
-      width="100%" 
-      viewBox="0 0 312 351" 
-      fill="none" 
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path 
-        vector-effect="non-scaling-stroke"
-        class="speech-outer"
-        d="M107.5 282L30.4 274.7C13.4 273.1 0.5 259 0.5 241.8V33.5C0.5 15.3 15.3 0.5 33.4 0.5C35.2 0.5 37 0.6 38.8 0.9L283.8 41.2C299.8 43.8 311.4 57.5 311.4 73.8V265.1C311.4 274 307.9 282.3 301.7 288.5C295.5 294.7 287.2 298.1 278.5 298.1C277.5 298.1 276.4 298 275.3 297.9L166.8 287.6L143.1 349.3L107.5 282Z" 
-        stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="10 10"
-        stroke-dashoffset="14px"
-        stroke-width="2"
-      />
-      <path
-        vector-effect="non-scaling-stroke"
-        class="speech-inner"
-        d="M37.6 8.8C22.3 6.3 8.5 18.1 8.5 33.5V241.8C8.5 254.7 18.3 265.5 31.1 266.7L112.5 274.4L142 330L161.6 279L276.1 290C290.8 291.4 303.5 279.9 303.5 265.1V73.8C303.5 61.6 294.6 51.1 282.6 49.1L37.6 8.8Z" 
-        fill="white" 
-        stroke-width="1"
-      />
-      <g class="speech-exclaim">
-        <text x="50" y="210" class="hi">
-          Hi
-        </text>
-        <text x="205" y="210" class="bang">
-          !
-        </text>
-      </g>
-      
-      <text></text>
-    </svg>
+  <div class="speech-container">
+    <div class="speech">
+      <svg 
+        width="100%" 
+        viewBox="0 0 312 351" 
+        fill="none" 
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path 
+          vector-effect="non-scaling-stroke"
+          class="speech-outer-1 orngLine"
+          d="M107.5 282L30.4 274.7C13.4 273.1 0.5 259 0.5 241.8V33.5C0.5 15.3 15.3 0.5 33.4 0.5C35.2 0.5 37 0.6 38.8 0.9L283.8 41.2C299.8 43.8 311.4 57.5 311.4 73.8V265.1C311.4 274 307.9 282.3 301.7 288.5C295.5 294.7 287.2 298.1 278.5 298.1C277.5 298.1 276.4 298 275.3 297.9L166.8 287.6L143.1 349.3L107.5 282Z" 
+          stroke-linecap="round" stroke-linejoin="round" 
+          stroke-width="2"
+        />
+        <path 
+          vector-effect="non-scaling-stroke"
+          class="speech-outer-2 orngLine"
+          d="M107.5 282L30.4 274.7C13.4 273.1 0.5 259 0.5 241.8V33.5C0.5 15.3 15.3 0.5 33.4 0.5C35.2 0.5 37 0.6 38.8 0.9L283.8 41.2C299.8 43.8 311.4 57.5 311.4 73.8V265.1C311.4 274 307.9 282.3 301.7 288.5C295.5 294.7 287.2 298.1 278.5 298.1C277.5 298.1 276.4 298 275.3 297.9L166.8 287.6L143.1 349.3L107.5 282Z" 
+          stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="10 10"
+          stroke-dashoffset="14px"
+          stroke-width="2"
+        />
+        <path
+          vector-effect="non-scaling-stroke"
+          class="speech-inner"
+          d="M37.6 8.8C22.3 6.3 8.5 18.1 8.5 33.5V241.8C8.5 254.7 18.3 265.5 31.1 266.7L112.5 274.4L142 330L161.6 279L276.1 290C290.8 291.4 303.5 279.9 303.5 265.1V73.8C303.5 61.6 294.6 51.1 282.6 49.1L37.6 8.8Z" 
+          fill="white" 
+          stroke-width="1"
+        />
+        <g class="speech-exclaim">
+          <text x="50" y="210" class="hi">
+            Hi
+          </text>
+          <text x="205" y="210" class="bang">
+            !
+          </text>
+        </g>
+        </svg>
+    </div>
   </div>
+  
 
   <h1>
     My name is Zach. I’m an integrative 
